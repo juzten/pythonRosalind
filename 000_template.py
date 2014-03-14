b39bfa957c9bc482d1507096b2be7e9add9becb0
juzten <juzten@gmail.com>
2014-03-13 20:29:29 -0400

    added 000_template.py file.

---
+++ 000_template.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#-----------------------------------------------------------------------------
+# Name:  Justin Spain
+# Email: juzten@gmail.com
+# Date:  
+# File:  .py
+#-----------------------------------------------------------------------------
+# Edit log
+# 11/17/2013 edit log here
+# 
+#-----------------------------------------------------------------------------
+# [TODO]
+# todo list here
+# 
+#-----------------------------------------------------------------------------
+
+
+# Rosalind bioinformatics exercises
+# Exersie: Counting DNA Nucleotides
+# URL:     http://rosalind.info/problems/dna/
+
+
+
+# Problem:
+
+
+
+
+# --notes
+
+#-----------------------------------------------------------------------------
+# Place any necessary functions below this.
+#-----------------------------------------------------------------------------
+def do_something(x):
+    # Place any line processing code here.
+    x += 1
+    return x
+
+
+#-----------------------------------------------------------------------------
+# Begin the main program.
+#-----------------------------------------------------------------------------
+# initialize variables
+x = 1
+
+
+print do_something(x)
+
+
+
+# test
+# py.test 002_rna.py
+def test_one():
+	x = 5
+	assert do_something(x) == 6
